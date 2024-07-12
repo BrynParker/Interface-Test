823d16e6113a3b745efb2bae2d3bcb47b87384f8
Bryn Parker <brynparker@me.com>
2024-07-12 14:52:39 -0500

    Add files via upload

---
+++ app/App_Code/Services/ApplicationServices.generated.cs
@@ -0,0 +1,25 @@
+﻿using RiverlandIndustries.Handlers;
+using RiverlandIndustries.Web;
+using System.Web.Configuration;
+
+namespace RiverlandIndustries.Services
+{
+    public class AppFrameworkConfig
+    {
+
+        public virtual void Initialize()
+        {
+            ApplicationServices.FrameworkAppName = "RL-Interface-TKS";
+            ApplicationServices.Version = "8.9.40.0";
+            ApplicationServices.HostVersion = "1.2.5.0";
+            var compilation = ((CompilationSection)(WebConfigurationManager.GetSection("system.web/compilation")));
+            var releaseMode = !compilation.Debug;
+            AquariumExtenderBase.EnableMinifiedScript = releaseMode;
+            AquariumExtenderBase.EnableCombinedScript = releaseMode;
+            ApplicationServices.EnableMinifiedCss = releaseMode;
+            ApplicationServices.EnableCombinedCss = releaseMode;
+            ApplicationServicesBase.AuthorizationIsSupported = false;
+            BlobFactoryConfig.Initialize();
+        }
+    }
+}
